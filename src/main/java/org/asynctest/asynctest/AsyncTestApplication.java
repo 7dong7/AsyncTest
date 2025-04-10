5b7c749cf575a9db4cec3001067883add87f29ac
7dong7 <tmdkdl777@gmail.com>
2025-04-10 20:00:16 +0900

    비동기 처리 테스트 프로젝트 생성

---
+++ src/main/java/org/asynctest/asynctest/AsyncTestApplication.java
@@ -0,0 +1,13 @@
+package org.asynctest.asynctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/asynctest/asynctest/AsyncTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asynctest.asynctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
